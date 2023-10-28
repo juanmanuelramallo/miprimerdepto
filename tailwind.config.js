98ccc962e4ce44e1abf04d2c23f10be9e5c13f43
Juan Manuel Ramallo <juanmanuelramallo@hey.com>
2023-10-28 14:48:32 -0300

    Configure tailwind and add ./bin/dev

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "_site/**/*.html",
+    "_site/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography')
+  ],
+}
